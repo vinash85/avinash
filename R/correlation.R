bca991223cd69cec467d92cb834f2bfd0262ccb7
vinash85 <avinash.sahu@gmail.com>
2019-07-22 09:04:48 -0400

    added correlation

---
+++ R/correlation.R
@@ -0,0 +1,27 @@
+
+#' calc.cor calulate correlation and approximate P-value of two matrix
+#'
+#' @param x : matrix
+#' @param y : matrix
+#' @param method : a character string indicating which correlation coefficient (or covariance) is to be computed. One of "pearson" (default), "kendall", or "spearman": can be abbreviated.
+#' @param method : matrix
+#' @param use : "an optional character string giving a method for computing covariances in the presence of missing values. This must be (an abbreviation of) one of the strings "everything", "all.obs", "complete.obs", "na.or.complete", or "pairwise.complete.obs".
+#'
+#' @return estimate=correlation coefficient, P=P-value
+#' @export
+#'
+#' @examples
+calc.cor = function(x, y, method = "spearman", use = "pairwise.complete.obs"){
+  op <- options(warn = (-1)) 
+  h = cor(x, y, method = method, use = use)
+  options(op) # reset the default value
+  aa = (!is.na(x)) + 0
+  bb = (!is.na(y)) + 0
+  npair = t(aa) %*% bb
+  P <- 2 * (1 - pt(abs(h) * sqrt(npair - 2)/sqrt(1 - 
+                                                   h * h), npair - 2))
+  P[abs(h) == 1] <- 0
+  P[is.na(P)] <-1
+  list(estimate=h, P = P)
+}
+
